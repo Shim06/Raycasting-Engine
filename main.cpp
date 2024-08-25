1f9193bf186471fc225283b0260e716a3665e26a
Shim Manaloto <shimmanaloto@gmail.com>
2024-08-26 01:57:23 +0800

    Implemented raycasting rendering, mouse input, and image loading

--- main.cpp
+++ main.cpp
@@ -23,16 +23,27 @@ int main(int, char**)
         SDL_Log("Error creating SDL_Renderer!");
         return 1;
     }
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags))
+    {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return 1;
+    }
     SDL_SetWindowMinimumSize(window, 640, 480);
     SDL_RenderSetLogicalSize(renderer, screen_width, screen_height);
+    SDL_SetRelativeMouseMode(SDL_TRUE);
+
+    std::vector<SDL_Texture*> texture;
+    texture.resize(1);
+    texture[0] = IMG_LoadTexture(renderer, "assets\\textures\\wall_ship_0.png");
+
 
     //player_speed = player_speed / (map_scale * cell_size);
     bool done = false;
-    player.pos.x = 2;
-    player.pos.y = 2;
+    player.pos.x = 1;
+    player.pos.y = 1;
     while (!done)
     {
-        old_time = SDL_GetTicks();
         if (handle_input(window) == -1) done = true;
 
         // Main Code
@@ -49,9 +60,10 @@ int main(int, char**)
         SDL_RenderDrawPoint(renderer, end_x, end_y);
         SDL_RenderDrawLine(renderer, player.global_pos.x, player.global_pos.y, end_x, end_y);
 
-        for (int16_t x = 512; x < screen_width; x++)
+        for (int16_t x = screen_width / 2; x < screen_width; x++)
         {
-            double camera_x = 2 * x / double(screen_width) - 1;
+            double camera_width = x - (screen_width / 2);
+            double camera_x = 2 * camera_width / double(screen_width / 2) - 1;
             vector2D ray_direction;
             ray_direction.x = player.direction.x + plane.x * camera_x;
             ray_direction.y = player.direction.y + plane.y * camera_x;
@@ -77,18 +89,104 @@ int main(int, char**)
 
             if (ray_direction.x < 0)
             {
-
+                step_x = -1;
+                side_dist.x = (player.pos.x - map_x) * delta_dist.x;
             }
             else
             {
-
+                step_x = 1;
+                side_dist.x = (map_x + 1.0 - player.pos.x) * delta_dist.x;
             }
+            if (ray_direction.y < 0)
+            {
+                step_y = -1;
+                side_dist.y = (player.pos.y - map_y) * delta_dist.y;
+            }
+            else
+            {
+                step_y = 1;
+                side_dist.y = (map_y + 1.0 - player.pos.y) * delta_dist.y;
+            }
+
+            while (hit == 0)
+            {
+                if (side_dist.x < side_dist.y)
+                {
+                    side_dist.x += delta_dist.x;
+                    map_x += step_x;
+                    side = 0;
+                }
+                else
+                {
+                    side_dist.y += delta_dist.y;
+                    map_y += step_y;
+                    side = 1;
+                }
+
+                if (map[map_y][map_x] > 0) hit = 1;
+            }
+
+            if (side == 0) perp_wall_dist = (side_dist.x - delta_dist.x);
+            else perp_wall_dist = (side_dist.y - delta_dist.y);
+
+            // Calculate the height of the line to draw on the screen
+            int line_height = (int)(screen_height / perp_wall_dist);
+
+            color blue = { 0, 0, 255, 255 };
+            if (side == 1) { blue.b /= 1.7; }
+            switch (map[map_y][map_x])
+            {
+            case 1:
+                SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, 255);
+                break;
+
+            case 2:
+                SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, 255);
+                break;
+
+            case 3:
+                SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, 255);
+                break;
+
+            case 4:
+                SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, 255);
+                break;
+
+            case 5:
+                SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, 255);
+                break;
+
+            case 6:
+                SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, 255);
+                break;
+
+            case 7:
+                SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, 255);
+                break;
+
+            case 8:
+                SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, 255);
+                break;
+            }
+
+            //calculate lowest and highest pixel to fill in current stripe
+            int draw_start = -line_height / 2 + screen_height / 2;
+            if (draw_start < 0) draw_start = 0;
+
+            int draw_end = line_height / 2 + screen_height / 2;
+            if (draw_end >= screen_height) draw_end = screen_height - 1;
+
+            SDL_RenderDrawLine(renderer, x, draw_start, x, draw_end);
         }
+
+        SDL_Rect destRect = { 100, 100, 64, 64 };
+        SDL_RenderCopy(renderer, texture[0], nullptr, &destRect);
         SDL_RenderPresent(renderer);
 
         current_time = SDL_GetTicks();
         frame_time = (current_time - old_time) / 1000.0;
-        //std::cout << "FPS: " << 1.0 / frame_time << "\n";
+        old_time = current_time;
+        std::cout << "FPS: " << 1.0 / frame_time << "\n";
     }
 
     SDL_DestroyRenderer(renderer);
@@ -129,6 +227,10 @@ int handle_input(SDL_Window*& window)
                 player_controls.move_down = true;
                 break;
 
+            case SDLK_ESCAPE:
+                return -1;
+                break;
+
             default: break;
             }
         }
@@ -162,6 +264,8 @@ int handle_input(SDL_Window*& window)
 
 void drawMap(SDL_Renderer *& renderer)
 {
+    if (map_scale_x <= 0) map_scale_x = 0.5;
+    if (map_scale_y <= 0) map_scale_y = 0.1;
     uint16_t x, y, pos_x, pos_y;
     for (y = 0; y < map_height; y++)
     {
@@ -201,44 +305,51 @@ void drawPlayer(SDL_Renderer*& renderer)
 
 void handle_player_movement()
 {
+    int motion_x, motion_y;
+    SDL_GetRelativeMouseState(&motion_x, &motion_y);
     double move_speed = player_speed * frame_time;
-    double rotation_speed = player_rotation_speed * frame_time;
     if (player_controls.move_up)
     {
         double location_x = player.pos.x + (move_speed * player.direction.x);
         double location_y = player.pos.y + (move_speed * player.direction.y);
-        std::cout << "x: " << (int)location_x << " " << "y: " << (int)location_y << "\n";
         if (map[(int)player.pos.y][(int)location_x] == false) player.pos.x = location_x;
         if (map[(int)location_y][(int)player.pos.x] == false) player.pos.y = location_y;
     }
     if (player_controls.move_down)
     {
-        std::cout << (player.global_pos.x / (cell_size * map_scale_x)) << "\n";
         double location_x = player.pos.x - (move_speed * player.direction.x);
         double location_y = player.pos.y - (move_speed * player.direction.y);
         if (map[(int)player.pos.y][(int)location_x] == false) player.pos.x = location_x;
         if (map[(int)location_y][(int)player.pos.x] == false) player.pos.y = location_y;
     }
-
-
     if (player_controls.move_left)
     {
+        double strafe_x = player.direction.y;
+        double strafe_y = -player.direction.x;
+        double location_x = player.pos.x - (move_speed * strafe_x);
+        double location_y = player.pos.y - (move_speed * strafe_y);
+        if (map[(int)player.pos.y][(int)location_x] == false) player.pos.x = location_x;
+        if (map[(int)location_y][(int)player.pos.x] == false) player.pos.y = location_y;
+    }
+    if (player_controls.move_right)
+    {
+        double strafe_x = player.direction.y;
+        double strafe_y = -player.direction.x;
+        double location_x = player.pos.x + (move_speed * strafe_x);
+        double location_y = player.pos.y + (move_speed * strafe_y);
+        if (map[(int)player.pos.y][(int)location_x] == false) player.pos.x = location_x;
+        if (map[(int)location_y][(int)player.pos.x] == false) player.pos.y = location_y;
+    }
+
+    if (motion_x != 0)
+    {
+        double rotation_speed = (motion_x * player_controls.mouse_sensitivity) * frame_time;
         double old_direction_x = player.direction.x;
         player.direction.x = player.direction.x * std::cos(-rotation_speed) - player.direction.y * std::sin(-rotation_speed);
         player.direction.y = old_direction_x * std::sin(-rotation_speed) + player.direction.y * std::cos(-rotation_speed);
 
         double old_plane_x = plane.x;
         plane.x = plane.x * std::cos(-rotation_speed) - plane.y * std::sin(-rotation_speed);
-        plane.y = plane.x * std::sin(-rotation_speed) + plane.y * std::cos(-rotation_speed);
-    }
-    if (player_controls.move_right)
-    {
-        double old_direction_x = player.direction.x;
-        player.direction.x = player.direction.x * std::cos(rotation_speed) - player.direction.y * std::sin(rotation_speed);
-        player.direction.y = old_direction_x * std::sin(rotation_speed) + player.direction.y * std::cos(rotation_speed);
-
-        double old_plane_x = plane.x;
-        plane.x = plane.x * std::cos(rotation_speed) - plane.y * std::sin(rotation_speed);
-        plane.y = old_plane_x * std::sin(rotation_speed) + plane.y * std::cos(rotation_speed);
+        plane.y = old_plane_x * std::sin(-rotation_speed) + plane.y * std::cos(-rotation_speed);
     }
 }
